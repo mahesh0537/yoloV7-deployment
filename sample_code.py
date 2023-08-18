1be1c2e96fd8b8104fb56721c88d4c03d5d3e0f9
mahesh <mail2mahesh0537@gmail.com>
2023-08-19 00:14:02 +0530

    '

--- sample_code.py
+++ sample_code.py
@@ -9,6 +9,6 @@ if __name__ =="__main__":
         os.makedirs(outDir)
     d = detector()
     test_img = cv2.imread('test/images/4 (13)_1649859983.jpg')
-    total_cars, img = d(test_img.copy())
+    total_cars, img = d(test_img.copy())            #calling the detector class with the image, and getting the total cars and the image with the bounding boxes
     print(f'Total cars: {total_cars}')
     cv2.imwrite(os.path.join(outDir, f'out {get_time()}.jpg'), img)
